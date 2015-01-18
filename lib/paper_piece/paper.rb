c755d226f08dedf448d38d4744243a2ea4124139
Andre Aubin <andre.aubin@lambdaweb.fr>
2015-01-18 11:32:53 +0100

    up

--- lib/paper_piece/paper.rb
+++ lib/paper_piece/paper.rb
@@ -17,21 +17,41 @@ module PaperPiece
       @stack.to_yaml
     end
 
-    def render
-
-    end
-
     def <<(value)
       @stack += [*value]
+      self
+    end
+
+    def +(value)
+      @stack += [*value]
+      self
+    end
+
+    def to_s
+      self.render
+    end
+
+
+    def render
+      buffer = []
+      connectors = PaperPiece.translations[PaperPiece.language]["connectors"]
+      p connectors
+      first, middle, last = cut @stack
+
+      buffer << connectors["first"].sample + first if first
+      middle.each{|part| buffer << connectors["middle"].sample + part} if middle.any?
+      buffer << connectors["last"].sample + last if last
+      return buffer
     end
 
     private
 
     def cut part
-      f = part.shift
-      l = part.pop
-      if part.any?
-        return f, part, l
+      _part = part.clone
+      f = _part.shift
+      l = _part.pop
+      if _part.any?
+        return f, _part, l
       else
         return f, nil, l
       end
